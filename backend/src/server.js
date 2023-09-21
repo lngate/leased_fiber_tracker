bccf19d8e167434ed4ecac6a968c0a6f4f9aa62d
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-09-21 23:19:37 +0300

    backend  create of folder structure

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    console.log('welcome to leased fibe test')
+})
+
+app.listen(8080, ()=>{
+    console.log("server is listening on port 8080")
+});
